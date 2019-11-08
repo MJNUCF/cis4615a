d83f06baa5119bf05dbec73fb65e2c2fb8a97559
MattNipper <32180597+MattNipper@users.noreply.github.com>
2019-11-07 23:23:25 -0500

    Corrected R04_STR03_J

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -17,17 +17,17 @@ public class R04_STR03_J {
     
     /*
      * Rule 04. Characters and Strings (STR)
-     * Incorrect code per:
+     * Correct code per:
      * https://wiki.sei.cmu.edu/confluence/display/java/STR03-J.+Do+not+encode+noncharacter+data+as+a+string
      *
      *Rule 04-STR03
      */
 	
     BigInteger x = new BigInteger("530500452766");
-    byte[] byteArray = x.toByteArray();
-    String s = new String(byteArray);
-    byteArray = s.getBytes();
-    x = new BigInteger(byteArray);
+    String s = x.toString();  // Valid character data
+    byte[] byteArray = s.getBytes();
+    String ns = new String(byteArray); 
+    x = new BigInteger(ns);
     
     System.out.println(x);
       
