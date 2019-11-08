7b3730d4794a38d93399aaa3d5cf0706fe8d87ba
MattNipper <32180597+MattNipper@users.noreply.github.com>
2019-11-07 23:22:10 -0500

    Corrected R00_IDS03_J

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -3,21 +3,16 @@
  *  Compilation:  javac R00_IDS03_J.java
  *  Execution:    java R00_IDS03_J
  *
+ *  logs user login attempts and sanatizes the input.
+ *
  ******************************************************************************/
 
+import java.io.*;
 import java.util.logging.Logger;
 
 public class R00_IDS03_J {
 
     public static void main(String[] args) {
-	    
-       /*
-	* Rule 00. Input Validation and Data Sanitization (IDS)
-	* Incorrect code per:
-	* https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input
-	*
-	* Rule 00-IDS03
-	*/
 	
         Logger logger = Logger.getLogger("R00_IDS03_J");
     
@@ -25,18 +20,30 @@ public class R00_IDS03_J {
         String username = "asdf123";
 
         if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
         } else {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitizeUser(username));
         }
         
         loginSuccessful = false;
-        username = "asdf123";
         
         if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
         } else {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitizeUser(username));
         }
+    }
+    
+    /*
+     * Rule 00. Input Validation and Data Sanitization (IDS)
+     * Corrected code per:
+     * https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input
+     *
+     * Rule 00-IDS03
+     */
+     
+    public String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9_]+", username))
+            ? username : "unauthorized user";
     }
 }
