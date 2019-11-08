08a109b849244b084b49b21d4cf538c91b906266
MattNipper <32180597+MattNipper@users.noreply.github.com>
2019-11-07 22:22:50 -0500

    Added citation

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -10,6 +10,14 @@ import java.util.logging.Logger;
 public class R00_IDS03_J {
 
     public static void main(String[] args) {
+	    
+       /*
+	* Rule 00. Input Validation and Data Sanitization (IDS)
+	* Incorrect code per:
+	* https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input
+	*
+	* Rule 00-IDS03
+	*/
 	
         Logger logger = Logger.getLogger("R00_IDS03_J");
     
