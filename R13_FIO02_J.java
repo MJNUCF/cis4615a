80f957308757ab55fdc6dfa08c973a5364c2649d
MattNipper <32180597+MattNipper@users.noreply.github.com>
2019-11-07 22:21:10 -0500

    Corrected file name; Added citation

--- R13_FIO02_J.java
+++ R13_FIO02_J.java
@@ -1,6 +1,6 @@
 /******************************************************************************
- *  Compilation:  javac R13_FIO002_J.java
- *  Execution:    java R13_FIO002_J
+ *  Compilation:  javac R13_FIO02_J.java
+ *  Execution:    java R13_FIO02_J
  *
  *  Creates a file and tries to delete it
  *
@@ -9,9 +9,17 @@
 import java.util.*;
 import java.io.*;
 
-public class R13_FIO002_J {
+public class R13_FIO02_J {
 
     public static void main(String[] args) {
+        
+    /*
+     * Rule 13. Input Output (FIO)
+     * Incorrect code per:
+     * https://wiki.sei.cmu.edu/confluence/display/java/FIO02-J.+Detect+and+handle+file-related+errors
+     *
+     *Rule 13-FIO02
+     */
     
         File file = new File(args[0]);
         file.delete();
