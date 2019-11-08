9f669c278b4d25aa1497b0b7d74f55a4546567e4
MattNipper <32180597+MattNipper@users.noreply.github.com>
2019-11-07 23:23:50 -0500

    Corrected R06_Met01_J

--- R06_Met01_J.java
+++ R06_Met01_J.java
@@ -28,7 +28,7 @@ public class R06_Met01_J {
 
     /*
      * Rule 06. Methods (MET)
-     * Incorrect code per:
+     * Corrected code per:
      * https://wiki.sei.cmu.edu/confluence/display/java/MET01-J.+Never+use+assertions+to+validate+method+arguments
      *
      *Rule 06-Met01
@@ -36,11 +36,14 @@ public class R06_Met01_J {
 
 
     public static int getAbsAdd(int x, int y) {
-        assert x != Integer.MIN_VALUE;
-        assert y != Integer.MIN_VALUE;
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException();
+        }
         int absX = Math.abs(x);
         int absY = Math.abs(y);
-        assert (absX <= Integer.MAX_VALUE - absY);
+        if (absX > Integer.MAX_VALUE - absY) {
+            throw new IllegalArgumentException();
+        }
         return absX + absY;
     }
 }
